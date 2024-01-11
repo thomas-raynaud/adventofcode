de2382f62e1e45993b14aad5d70fe54155eea4a7
thomas-raynaud <raynaud.thomas@gmail.com>
2024-01-11 21:26:05 +0100

    2023 day 9 code cleaning

--- 2023/day09.py
+++ 2023/day09.py
@@ -5,23 +5,17 @@ for line_str in open("input_d09"):
     line = [ int(x) for x in line_str.rstrip().split(" ") ]
     all_deltas = list()
     deltas_all_zero = False
-    prev_deltas = line
     all_deltas.append(line)
     while not deltas_all_zero:
         deltas_all_zero = True
         deltas = list()
-        for i in range(0, len(prev_deltas) - 1):
-            delta = prev_deltas[i + 1] - prev_deltas[i]
-            deltas.append(delta)
-            if delta != 0:
-                deltas_all_zero = False
+        for i in range(0, len(all_deltas[-1]) - 1):
+            deltas.append(all_deltas[-1][i + 1] - all_deltas[-1][i])
+            deltas_all_zero = deltas_all_zero and deltas[-1] == 0
         all_deltas.append(deltas)
-        prev_deltas = deltas
 
-    all_deltas[-1].append(0)
     for i in range(len(all_deltas) - 2, -1, -1):
         all_deltas[i].append(all_deltas[i][-1] + all_deltas[i + 1][-1])
-        # Part 2
         all_deltas[i].insert(0, all_deltas[i][0] - all_deltas[i + 1][0])
 
     output_p1 += all_deltas[0][-1]
